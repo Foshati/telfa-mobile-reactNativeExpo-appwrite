66a593d990c7ba01d04bddcafc95b2ccbb53aa4b
Foshati <amirrezafoshati@gmail.com>
2024-06-19 23:42:20 +0330

    add icon tabBar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function _layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="category" />
-      <Tabs.Screen name="search" />
-      <Tabs.Screen name="bookmarks" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="category"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="category" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="bookmark" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
